fd11a0c0ef47fb6b91a976cf603ba805c84da9f2
Fons <alfonso@sosiax.com>
2020-03-11 16:24:02 +0100

    testing...

--- diskless-centos-minimal.sh
+++ diskless-centos-minimal.sh
@@ -21,7 +21,7 @@ cd $ROOTDISK
 
 #yum --installroot=$ROOTDISK/ --enablerepo=elrepo install basesystem filesystem bash passwd dhclient yum openssh-server openssh-clients nfs-utils ipa-client cronie-anacron selinux-policy-targeted vim-minimal kernel-lt
 #yum -y install --releasever=7 --installroot=$ROOTDISK  basesystem filesystem bash passwd dhclient yum openssh-server openssh-clients nfs-utils ipa-client vim-minimal util-linux shadow-utils
-yum -y install --releasever=/ -C --enablerepo=elrepo-kernel --installroot=$ROOTDISK  basesystem filesystem bash passwd dhclient openssh-server openssh-clients nfs-utils vim-minimal util-linux shadow-utils kernel-lt net-tools
+yum -y install --releasever=/ --enablerepo=elrepo-kernel --installroot=$ROOTDISK  basesystem filesystem bash passwd dhclient openssh-server openssh-clients nfs-utils vim-minimal util-linux shadow-utils kernel-lt net-tools
 
 # Configuring yum 
 echo "diskspacecheck=0" >> $ROOTDISK/etc/yum.conf
@@ -34,7 +34,7 @@ cp /etc/profile.d/icmat.sh $ROOTDISK/etc/profile.d/icmat.sh
 #cp /etc/yum.repos.d/elrepo.repo $ROOTDISK/etc/yum.repos.d/elrepo.repo
 # Coping rc.local
 cp -f $SCRITP_DIR/rc.local $ROOTDISK/rc.local
-mkdir $ROOTDISK/root/.ssh 
+mkdir -p $ROOTDISK/root/.ssh 
 chmod 700 $ROOTDISK/root/.ssh 
 cp -pr /root/.ssh/authorized_keys /root/.ssh/known_hosts $ROOTDISK/root/.ssh 
 
