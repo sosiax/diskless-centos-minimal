c1d069f15402210c43d5386d3ab25c47288917bc
Fons <alfonso@sosiax.com>
2020-03-13 12:08:02 +0100

    Moving to function drived scriptgit add -A!

--- diskless-centos-minimal.sh
+++ diskless-centos-minimal.sh
@@ -1,4 +1,38 @@
 #!/bin/sh
+
+#  diskless-centos-minimal.sh
+#  
+#  Copyright 2020 Alfonso Núñez Salgadp <anunez@icmat.es>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+
+function usage { 
+  echo "Usage: $0 <COMMAND> <PATH> [DEST]" 1>&2; 
+  echo "COMMANDS:" 1>&2; 
+  echo "  clean     : Yum clean " 1>&2; 
+  echo "  mkinitrd  : create initrd as is in directory " 1>&2; 
+  echo "  mkminimal : create initrd removing all un neaded files (nfs mount) " 1>&2; 
+  exit 1; 
+  }
+
+#=======================================================================
+# GLOBAL VARIABLES
+#=======================================================================
 TFTPBOOT_SRV="master:/var/lib/tftpboot/centos7/"
 SCRITP_DIR=`readlink -f $(dirname $0)`
 LOG=$SCRITP_DIR/$(basename $0).log
@@ -6,19 +40,30 @@ export ROOTDISK=/var/lib/diskless/centos7-minimal/
 export DISKIMAGE=/root/tmp/diskless-image/diskless.img
 export VMLINUZIMAGE=$(dirname $DISKIMAGE)/vmlinuz-$(basename $DISKIMAGE)
 
+#============================
+#  MkInitrd
+#============================
 function MkInitrd {
   cd $ROOTDISK
   time find | cpio -oc | pigz -9 > $DISKIMAGE
   scp $DISKIMAGE $TFTPBOOT_SRV
 }
 
+#============================
+#  MkInitrd
+#============================
 function YumClean() {
   echo "Cleanning ..." >&2
   yum clean all --installroot=$ROOTDISK
   rm -fr $ROOTDISK/var/cache/yum
 }
 
+#============================
+#  MkInitrd
+#============================
 function ReduceDiskSpace () {
+  [[ -e $DISK_PATH/diskless.var.lib.rpm.tgz ]] && echo "Removing old TAR : $DISK_PATH/diskless.var.lib.rpm.tgz" && rm -f $DISK_PATH/diskless.var.lib.rpm.tgz
+  
   cd $ROOTDISK
   BAK_LIST="var/lib/rpm var/lib/yum usr/lpp usr/share/man usr/share/doc boot/"
   tar -I pigz -cf $SCRITP_DIR/diskless.var.lib.rpm.tgz $BAK_LIST
@@ -29,6 +74,9 @@ function ReduceDiskSpace () {
   cd -
 }
 
+#============================
+#  MkInitrd
+#============================
 function ExpandDiskSpace () {
   cd $ROOTDISK
   tar -I pigz -xf $SCRITP_DIR/var.tgz var/
@@ -36,94 +84,105 @@ function ExpandDiskSpace () {
   cd -
 }
 
+#============================
+#  MkInitrd
+#============================
 function InstallSystem () {
-  
   echo -n "Installing system ..... "
-  
   yum group -y install --releasever=/ --installroot=$ROOTDISK "Instalación mínima" &>> $LOG
   cp /etc/yum.repos.d/elrepo.repo $ROOTDISK/etc/yum.repos.d/elrepo.repo
   yum -y install --releasever=/ --enablerepo=elrepo-kernel --installroot=$ROOTDISK  \
      basesystem filesystem bash passwd \
      dhclient openssh-server openssh-clients nfs-utils yum polkit ipa-client\
      vim-minimal util-linux shadow-utils kernel-lt net-tools cronie-anacron &>> $LOG
-  
   # Configuring yum 
   echo "diskspacecheck=0" >> $ROOTDISK/etc/yum.conf
   echo "keepcache=0" >> $ROOTDISK/etc/yum.conf
-
   echo "Done"
 }
 
+function PrepareSystem(){
+  # coping script files
+  cp /etc/profile.d/icmat.sh $ROOTDISK/etc/profile.d/icmat.sh	
+  cp -f $SCRITP_DIR/diskless-boot.sh $ROOTDISK/root/
+  cp -f $SCRITP_DIR/diskless-boot.service $ROOTDISK/usr/lib/systemd/system/ 
+  cp -f $SCRITP_DIR/chroot_cmds.sh $ROOTDISK/root/
+  chmod +x $ROOTDISK/root/chroot_cmds.sh 
+  
+  # Adding ssh passthrought
+  mkdir -p $ROOTDISK/root/.ssh 
+  chmod 700 $ROOTDISK/root/.ssh 
+  cp -pr /root/.ssh/authorized_keys /root/.ssh/known_hosts $ROOTDISK/root/.ssh 
+  
+  # Executing chroot commnads
+  chroot $ROOTDISK sh -x /root/chroot_cmds.sh 
+}
+#=============================================
+#  Main
+#=============================================
+
+#Getting posible options (not enabled options)
+while getopts ":a:" opt; do
+  case $opt in
+    a)
+      echo "-a was triggered, Parameter: $OPTARG" >&2
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+    *)
+    echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+  esac
+done
+
+command=$1
+
+# Checking fs mounted
 if mount $ROOTDISK &> /dev/null 
 then 
   echo "!!!! $ROOTDISK already mounted"
   read -n1 -r -p "Press any key to continue..." key
+else
+  mkdir -p $ROOTDISK
+  # Speed up image building -- Atention!
+  mount -t tmpfs -o size=4G,nr_inodes=400k tmpfs $ROOTDISK
 fi
 
-mkdir -p $ROOTDISK/var/cache/yum
-rsync -rAa $SCRITP_DIR/cache/yum $ROOTDISK/var/cache/yum 1> $LOG
-
-mkdir -p $ROOTDISK
-
-# Speed up image building -- Atention!
-mount -t tmpfs -o size=4G,nr_inodes=400k tmpfs $ROOTDISK
-
-#~ cd $ROOTDISK
-#~ yum group -y install --releasever=/ --installroot=$ROOTDISK "Instalación mínima"
-
-# install base binaries.  If you're not using puppet and IPA, this list can be trimmed.
-
-InstallSystem
-#~ echo -n "Installing system ..... "
-#~ yum -y install --releasever=/ --enablerepo=elrepo-kernel --installroot=$ROOTDISK  \
-#~ basesystem filesystem bash passwd \
-#~ dhclient openssh-server openssh-clients nfs-utils yum polkit \
-#~ vim-minimal util-linux shadow-utils kernel-lt net-tools cronie-anacron &>> $LOG
-#~ echo "Done"
-
-#~ # Configuring yum 
-#~ echo "diskspacecheck=0" >> $ROOTDISK/etc/yum.conf
-#~ echo "keepcache=0" >> $ROOTDISK/etc/yum.conf
-
-# coping script files
-cp /etc/profile.d/icmat.sh $ROOTDISK/etc/profile.d/icmat.sh	
-cp -f $SCRITP_DIR/diskless-boot.sh $ROOTDISK/root/
-cp -f $SCRITP_DIR/diskless-boot.service $ROOTDISK/usr/lib/systemd/system/ 
-cp -f $SCRITP_DIR/chroot_cmds.sh $ROOTDISK/root/
-chmod +x $ROOTDISK/root/chroot_cmds.sh 
-
-# Adding ssh passthrought
-mkdir -p $ROOTDISK/root/.ssh 
-chmod 700 $ROOTDISK/root/.ssh 
-cp -pr /root/.ssh/authorized_keys /root/.ssh/known_hosts $ROOTDISK/root/.ssh 
-
-# Executing chroot commnads
-chroot $ROOTDISK sh -x /root/chroot_cmds.sh 
-
-# Getting kernel
-#~ cp $ROOTDISK/boot/vmlinuz-* $VMLINUZIMAGE
-#~ chmod 755 $VMLINUZIMAGE
-
-##### Cleaning up
-ReduceDiskSpace
-#~ rm -fr $ROOTDISK/boot/vmlinuz-* $ROOTDISK/boot/vmlinuz-*
-#~ rm -fr $ROOTDISK/boot/vmlinuz-* $ROOTDISK/boot/init*
-#~ # Atention !!!
-#~ rm -fr $ROOTDISK/usr/lib/firmware
-#~ rm -fr $ROOTDISK/usr/share/man/
-#~ rm -fr $ROOTDISK/usr/share/doc
-
-#~ # Keeping cache files
-#~ mkdir -p $SCRITP_DIR/cache/yum
-#~ rsync -rAav $ROOTDISK/var/cache/yum $SCRITP_DIR/cache/yum 1>> $LOG
-#~ rm -fr $ROOTDISK/var/cache/yum $ROOTDISK/var/lib/yum/*
+# Executing commnad
+case $command in
+  clean )
+    YumClean
+    ;;
+  mkinitrd )
+    echo "mkinitrd" >&2
+    MkInitrd 
+    ;;
+  mkminimal )
+    YumClean
+    ReduceDiskSpace
+    MkInitrd 
+    ExpandDiskSpace
+    ;;
+  install )
+    InstallSystem
+    ;;
+  preapare )
+    IPrepareSystem
+    ;;
+  *)
+    echo "Inavalid command : $command" >&2
+    usage
+    ;;
+esac
 
 
 
-#~ read -n1 -r -p "Press any key to continue..." key
-MkInitrd
-#~ cd $ROOTDISK 
-#~ find | cpio -oc | gzip -9 > $DISKIMAGE 2>> $LOG
-#~ chmod 644 $DISKIMAGE
 
 
