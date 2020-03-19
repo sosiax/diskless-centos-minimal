526cb48773bff3dbcf4be1d2e76f43bf9e2fa6e1
Fons <alfonso@sosiax.com>
2020-03-19 12:01:28 +0100

    Making ro root available

--- diskless-boot.sh
+++ diskless-boot.sh
@@ -12,11 +12,44 @@
 
 touch /var/lock/subsys/diskless-boot
 
-dhclient
+fail(){
+	echo -e "$1"
+	exit 1
+}
+
+# load module
+modprobe overlay || fail "ERROR: missing overlay kernel module"
+
+# create a writable fs to then create our mountpoints
+mount -t tmpfs tmpfs /mnt || fail "ERROR: could not create a temporary filesystem to mount the base filesystems for overlayfs"
+mkdir -p /mnt/cache/
+
+#look for fscache LABEL
+cache_dev=`blkid -L fscache`
+if [ -z $cache_dev ]
+then 
+	# create a writable fs to then create our mountpoints
+	mount  -o user_xattr $cache_dev /mnt/cache/ || fail "ERROR: could not create a temporary filesystem to mount the base filesystems for overlayfs"
+	mkdir -p /mnt/cache/fscache
+	service cachefilesd restart 
+else
+
+
+DIRLIST="/root /var /etc"
+for fs in $DIRLIST
+do
+  fsname=`echo $fs | tr '/' '-'`
+  mkdir -p /mnt/cache/overlay/$fs/up
+  mkdir -p /mnt/cache/overlay/$fs/work
+  mount -t overlay overlay$fsname -o lowerdir=$fs,upperdir=/mnt/cache/overlay/$fs/up,workdir=/mnt/cache/overlay/$fs/work $fs
+done
+
+#Check IP
+ip=`ip add | grep -ohE "192.168.([0-9]{1,3}[\.]){1}[0-9]{1,3}" | grep -v 255` || dhclient
 
 #TODO : check for cache device 
 echo "nfs-lustre.icmat.es:/mnt/lustre_fs          /LUSTRE  nfs     rw,hard,intr,rsize=8192,wsize=8192,timeo=14,nosharecache,fsc=lustre 1 1" >> /etc/fstab
-echo "192.168.1.133:/var/lib/diskless/centos7/usr /usr     nfs     ro,hard,intr,rsize=8192,wsize=8192,timeo=14,nosharecache,fsc=usr    1 1" >> /etc/fstab
+#echo "192.168.1.133:/var/lib/diskless/centos7/usr /usr     nfs     ro,hard,intr,rsize=8192,wsize=8192,timeo=14,nosharecache,fsc=usr    1 1" >> /etc/fstab
 
 mount -a
 
